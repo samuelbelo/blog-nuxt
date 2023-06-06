7f33d7a8c283b5ee88e8b2655d75db9125085f75
Samuel Belo <samuel.belo@al.infnet.edu.br>
2023-06-06 17:00:57 +0100

    first commit

---
+++ app.config.ts
@@ -0,0 +1,41 @@
+export default defineAppConfig({
+  alpine: {
+    title: 'Alpine',
+    description: 'The minimalist blog theme',
+    image: {
+      src: '/social-card-preview.png',
+      alt: 'An image showcasing my project.',
+      width: 400,
+      height: 300
+    },
+    header: {
+      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      // logo: {
+      //   path: '', // path of the logo
+      //   pathDark: '', // path of the logo in dark mode, leave this empty if you want to use the same logo
+      //   alt: '' // alt of the logo
+      // }
+    },
+    footer: {
+      credits: {
+        enabled: false, // possible value are : true | false
+        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+      },
+      navigation: true, // possible value are : true | false
+      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+    },
+    socials: {
+      twitter: 'samuelllbelo',
+      instagram: 'samuellbelo',
+      linkedin: {
+        icon: 'uil:linkedin',
+        label: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/samuelbelo/'
+      }
+    },
+    form: {
+      successMessage: 'Message sent. Thank you!'
+    }
+  }
+})
